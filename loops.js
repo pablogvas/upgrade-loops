c30c584119756131a4ca387f207015f13dcaec76
PabloGarciaUH <pablo.garcia@bootcamp-upgrade.com>
2022-12-09 20:31:32 +0100

    Iteración#1

---
+++ loops.js
@@ -0,0 +1,10 @@
+// Iteración #1 //
+
+const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta'];
+
+for(let i = 0; i< products.length; i++) {
+    if(products.includes("Camiseta")) {
+        console.log(products[i]);
+    }
+}
+
