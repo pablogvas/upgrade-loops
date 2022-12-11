f52f6aae847fe259e181d183d0ecbe85c6211811
PabloGarciaUH <pablo.garcia@bootcamp-upgrade.com>
2022-12-11 14:14:49 +0100

    Iteración#5

--- loops.js
+++ loops.js
@@ -57,3 +57,16 @@ for(let dato in alien) {
     console.log(dato + ": " + alien[dato]);
 }
 
+
+// Iteración #5 //
+
+const placesToTravel2 = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'}, {id: 40, name: 'Santander'}, {id: 44, name: 'Filipinas'}, {id: 59, name: 'Madagascar'}]
+
+for(i=0; i<placesToTravel2.length; i++) {
+    if(placesToTravel2[i].id === 11 || placesToTravel2[i].id === 40) {
+        placesToTravel2.splice(i, 1);
+    }
+    console.log(placesToTravel2[i]);
+}
+
+
